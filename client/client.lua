6e6d283d9950c3d4a26fbcf97c8ed49da34063f5
Jellyton <20498875+jellyton255@users.noreply.github.com>
2025-01-22 14:14:17 -0800

    refactor(client): entirely rewrite player data handling to properly update data as it changes using a single source of truth

--- client/client.lua
+++ client/client.lua
@@ -1,92 +1,87 @@
-local config = require("config")
+local config = require('config')
 local JOBS = exports.qbx_core:GetJobs()
 
 local PanelOpen = false
 local Loaded = false
 
-local function getJobsData(jobs)
+---@class MultiJobGradeData
+---@field name string
+---@field level number
+
+---@class MultiJobData
+---@field name string
+---@field label string
+---@field payment number
+---@field isboss boolean
+---@field onduty boolean
+---@field grade MultiJobGradeData
+---@field workType 'salary' | 'contract'
+
+---Get job data to be transmitted to the UI
+---@param job string
+---@param grade number
+---@return MultiJobData?
+local function getJobData(job, grade)
+	local jobEntryData = JOBS[job] ---@type Job
+
+	if not jobEntryData then
+		lib.print.error('Job not found, removing: ' .. job)
+		TriggerServerEvent('EF-MultiJob:Server:RemoveJob', job)
+		return
+	end
+
+	if not jobEntryData.grades[grade] then
+		grade = #jobEntryData.grades
+		lib.print.error('Job not found, removing: ' .. job)
+		return
+	end
+
+	return {
+		name = job,
+		label = jobEntryData.label,
+		payment = jobEntryData.grades[grade].payment,
+		isboss = jobEntryData.grades[grade].isboss or false,
+		onduty = QBX.PlayerData.job.onduty or jobEntryData.defaultDuty or false,
+		grade = {
+			name = jobEntryData.grades[grade].name,
+			level = grade
+		},
+		workType = config.salaryJobs[job] and 'salary' or 'contract'
+	}
+end
+
+local function getFormattedJobs(jobs)
 	local formattedJobs = {}
 
 	for job, grade in pairs(jobs or QBX.PlayerData.jobs) do
-		if not JOBS[job] then
-			lib.print.error("Job not found, removing: " .. job)
-			TriggerServerEvent("EF-MultiJob:Server:RemoveJob", job)
+		local jobData = getJobData(job, grade)
 
-			goto continue
+		if jobData then
+			formattedJobs[#formattedJobs + 1] = jobData
 		end
-
-		formattedJobs[#formattedJobs + 1] = {
-			name = job,
-			label = JOBS[job].label,
-			payment = JOBS[job].grades[grade].payment,
-			isboss = JOBS[job].grades[grade].isboss or false,
-			grade = {
-				name = JOBS[job].grades[grade].name,
-				level = grade
-			},
-			workType = config.salaryJobs[job] and "salary" or "contract"
-		}
-
-		:: continue ::
 	end
 
 	return formattedJobs
 end
 
 function Initialize()
-	SendReactMessage("setJobs", getJobsData())
-
-	local jobData = QBX.PlayerData.job
-	jobData.workType = config.salaryJobs[jobData.name] and "salary" or "contract"
-
-	SendReactMessage("setCurrentJob", jobData)
+	SendReactMessage('setJobs', getFormattedJobs())
+	SendReactMessage('setCurrentJob', getJobData(QBX.PlayerData.job.name, QBX.PlayerData.job.grade.level))
 end
 
-RegisterNetEvent('QBCore:Client:OnJobUpdate', function(job, oldJobs)
-	lib.waitFor(function()
-		lib.print.debug(QBX.PlayerData.job.name, job.name)
-		if QBX.PlayerData.job.name == job.name then return true end
-	end, nil, 5000)
-
-	local jobData = QBX.PlayerData.job
-	jobData.workType = config.salaryJobs[jobData.name] and "salary" or "contract"
-
-	SendReactMessage("setCurrentJob", jobData)
-end)
-
-RegisterNetEvent('qbx_core:client:onGroupUpdate', function(jobName, grade, oldJobs)
-	lib.waitFor(function()
-		lib.print.debug(QBX.PlayerData.job.name, jobName)
-		if QBX.PlayerData.job.name == jobName then return true end
-	end, nil, 5000)
-
-	local jobData = QBX.PlayerData.job
-	jobData.workType = config.salaryJobs[jobData.name] and "salary" or "contract"
-
-	SendReactMessage("setCurrentJob", jobData)
-end)
-
 RegisterNetEvent('QBCore:Player:SetPlayerData', function(value)
-	SendReactMessage("setJobs", getJobsData(value.jobs))
+	SendReactMessage('setJobs', getFormattedJobs(value.jobs))
+
+	local jobData = getJobData(value.job.name, value.job.grade.level)
+	jobData.onduty = value.job.onduty
+
+	SendReactMessage('setCurrentJob', jobData)
 end)
-
-RegisterNetEvent('QBCore:Client:SetDuty', function(duty)
-	lib.waitFor(function()
-		lib.print.debug(QBX.PlayerData.job.onduty, duty)
-		if QBX.PlayerData.job.onduty == duty then return true end
-	end, nil, 5000)
-
-	local jobData = QBX.PlayerData.job
-	jobData.workType = config.salaryJobs[jobData.name] and "salary" or "contract"
-
-	SendReactMessage("setCurrentJob", jobData)
-end)
-
 
 -- NUI Handlers
 local function toggleNuiFrame(shouldShow)
 	if not Loaded then
-		lib.print.error("NUI not loaded yet...")
+		lib.print.error('NUI not loaded yet...')
 		return
 	end
 
@@ -96,29 +91,29 @@ local function toggleNuiFrame(shouldShow)
 		PanelOpen = false
 	end
 
-	SendReactMessage("setVisible", shouldShow)
+	SendReactMessage('setVisible', shouldShow)
 	SetNuiFocus(shouldShow, shouldShow)
 end
 
 -- Callbacks
-RegisterNUICallback("selectJob", function(job, cb)
-	lib.print.debug("Selecting Job: " .. job)
+RegisterNUICallback('selectJob', function(job, cb)
+	lib.print.debug('Selecting Job: ' .. job)
 
-	TriggerServerEvent("EF-MultiJob:Server:SelectJob", job)
+	TriggerServerEvent('EF-MultiJob:Server:SelectJob', job)
 	cb(1)
 end)
 
 RegisterNUICallback('removeJob', function(job, cb)
-	lib.print.debug("Removing Job: " .. job)
+	lib.print.debug('Removing Job: ' .. job)
 
-	TriggerServerEvent("EF-MultiJob:Server:RemoveJob", job)
+	TriggerServerEvent('EF-MultiJob:Server:RemoveJob', job)
 	cb(1)
 end)
 
 RegisterNUICallback('setJobDuty', function(status, cb)
-	lib.print.debug("Setting Job Duty Status: " .. tostring(status))
+	lib.print.debug('Setting Job Duty Status: ' .. tostring(status))
 
-	TriggerServerEvent("EF-MultiJob:Server:SetDutyStatus", status)
+	TriggerServerEvent('EF-MultiJob:Server:SetDutyStatus', status)
 	cb(1)
 end)
 
@@ -126,22 +121,22 @@ AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
 	Initialize()
 end)
 
-RegisterNUICallback("Loaded", function(data, cb)
+RegisterNUICallback('Loaded', function(data, cb)
 	Loaded = true
 	cb(1)
 	if not LocalPlayer.state.isLoggedIn then return end
 	Initialize()
 end)
 
-RegisterNUICallback("hideFrame", function(_, cb)
+RegisterNUICallback('hideFrame', function(_, cb)
 	cb(1)
 	toggleNuiFrame(false)
 	PanelOpen = false
 
-	lib.print.debug("Hide NUI frame")
+	lib.print.debug('Hide NUI frame')
 end)
 
-RegisterCommand("toggleJobMenu", function()
+RegisterCommand('toggleJobMenu', function()
 	toggleNuiFrame(not PanelOpen)
 end)
-RegisterKeyMapping('toggleJobMenu', "Job Menu", "keyboard", "F9")
+RegisterKeyMapping('toggleJobMenu', 'Job Menu', 'keyboard', 'F9')
